b013ea479cf695132b4cbcb51eca2be9e75255d0
ali <ali.ergul@pusula.net.tr>
2024-11-29 16:08:32 +0300

    task is completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,59 @@
+import { useState } from "react";
 import "./index.css";
+
 function App() {
-  return <div>test</div>;
+  const [dots, setDots] = useState([]);
+  const [removedDots, setRemovedDots] = useState([]);
+
+  const handleClick = (e) => {
+    setDots((dots) => [
+      ...dots,
+      {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    ]);
+    setRemovedDots([]);
+  };
+
+  const handleBack = (e) => {
+    e.stopPropagation();
+    const tempDots = [...dots];
+    const lastDot = tempDots.pop();
+    setRemovedDots((data) => [...data, lastDot]);
+    setDots(tempDots);
+  };
+
+  const handleForward = (e) => {
+    e.stopPropagation();
+    const tempRemoved = [...removedDots];
+    const forwardItem = tempRemoved.pop();
+    setDots((data) => [...data, forwardItem]);
+    setRemovedDots(tempRemoved);
+  };
+
+  return (
+    <div className="screen" onClick={(e) => handleClick(e)}>
+      <div className="buttons">
+        <button disabled={dots.length === 0} onClick={handleBack}>
+          Geri
+        </button>
+        <button disabled={removedDots.length === 0} onClick={handleForward}>
+          İleri
+        </button>
+      </div>
+
+      {dots?.map((dot, i) => (
+        <span
+          key={i}
+          style={{
+            left: `${dot.x}px`,
+            top: `${dot.y}px`,
+          }}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
